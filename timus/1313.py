5b52e424d5e0cf8f59a9c02101ffadcd6b84e08c
ubivas <ubivasyl@gmail.com>
2015-11-26 19:17:43 +0200

    init

---
+++ timus/1313.py
@@ -0,0 +1,17 @@
+import sys
+
+# How to define two-dimensional array in python
+A = [][]
+
+N = int(raw_input())
+
+s =  raw_input().split(' ')
+for i in range(0,N):
+	for j in range(0,N):
+    	A[i][j] = int(s[j])
+
+for i in range(1,N+1):
+	for j in range(0,i):
+	    print(A[][])
+
+    	
